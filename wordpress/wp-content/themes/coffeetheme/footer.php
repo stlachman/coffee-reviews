0105453636d8abfff5ae12f96c4b21bcf6bdf7f3
Seth Lachman <lachmanseth17@gmail.com>
2017-03-26 18:43:25 -0400

    Add coffee review home page

---
+++ wordpress/wp-content/themes/coffeetheme/footer.php
@@ -0,0 +1,12 @@
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>    
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <?php wp_footer(); ?>
---
+++ wordpress/wp-content/themes/coffeetheme/index.php
@@ -0,0 +1,35 @@
+
+<?php get_header(); ?>
+  
+<h1 class="starter-template">Coffee Reviews!</h1>
+            <p class="lead">Check Out Our Newest Coffee!</p>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Name of Coffee</th>
+                        <th>Average Review</th>
+                        <th>Number of Ratings</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><a href="coffee-review.html">Ethiopian</a></td>
+                        <td>3.5</td>
+                        <td>43</td>
+                    </tr>
+                     <tr>
+                        <td><a href="#">Rwanda</a></td>
+                        <td>3.7</td>
+                        <td>32</td>
+                    </tr>
+                     <tr>
+                        <td><a href="#">Colombia</a></td>
+                        <td>3.3</td>
+                        <td>55</td>
+                    </tr>
+                    </tbody>
+            </table>
+      
+ <?php get_footer(); ?>
+  </body>
+</html>
